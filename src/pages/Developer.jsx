ebf550b51b6676f48b9cd71d2b8082bffb3cd74c
webstoryboy <webstupids@gmail.com>
2023-10-15 14:25:07 +0900

    페이지 SEO 작업

--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
-const Developor = () => {
+const Developer = () => {
     return (
-        <Main>
-            Developor
+        <Main 
+            title = "추천 개발자"
+            description="오늘의 추천 개발자 유튜버입니다.">
+            Developer
         </Main>
     )
 }
 
-export default Developor
+export default Developer
--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
-const Video = () => {
+const Today = () => {
     return (
-        <Main>
-            Video
+        <Main 
+            title = "추천 영상"
+            description="오늘의 추천 유튜브 영상입니다.">
+            Today
         </Main>
     )
 }
 
-export default Video
+export default Today
