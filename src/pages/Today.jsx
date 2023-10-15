a418c20dd33257f4a43b44bbcd3547888e66ea90
webstoryboy <webstupids@gmail.com>
2023-10-15 16:24:41 +0900

    추천 영상 작업

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
+import { todayText } from '../data/today'
+import { Link } from 'react-router-dom'
+
 const Today = () => {
     return (
         <Main 
             title = "추천 영상"
             description="오늘의 추천 유튜브 영상입니다.">
-            Today
+            
+            <section>
+                <h2>🥰 오늘의 추천 개발자입니다.</h2>
+
+                {todayText.map((today, key) => (
+                    <div className='today__inner' key={key}>
+                        <div className='today__thumb play__icon'>
+                            <Link to={today.page}>
+                                <img src={today.img} alt={today.title} />
+                            </Link>
+                        </div>
+                        <div className='today__text'>
+                            <span className='today'>today!</span>
+                            <h3 className='title'>
+                                <Link to={today.page}>
+                                    {today.title}
+                                </Link>
+                            </h3>
+                            <p className='desc'>{today.desc}</p>
+                            <div className='info'>
+                                <span className='author'>
+                                    <Link to={`/channel/${today.channelId}`}>
+                                        {today.author}           
+                                    </Link>
+                                </span>
+                                <span className='date'>{today.date}</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </section>
+
+            
         </Main>
     )
 }
