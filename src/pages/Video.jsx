075a6fbebc6fc9ebed0c58de43e2b2094e8a10d7
webstoryboy <webstupids@gmail.com>
2023-10-15 20:57:02 +0900

    메인 섹션 나머지 콘텐츠 작업

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
-const Today = () => {
+const Video = () => {
     return (
         <Main 
             title = "추천 영상"
             description="오늘의 추천 유튜브 영상입니다.">
-            Today
+            Video
         </Main>
     )
 }
 
-export default Today
+export default Video
