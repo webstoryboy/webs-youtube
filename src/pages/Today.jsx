021c22752f3e2e8d6bd388c66e6c22fabf2d0ac5
webstoryboy <webstupids@gmail.com>
2023-10-16 22:15:34 +0900

    나머지 페이지 작업

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
 
 import { todayText } from '../data/today'
 import { Link } from 'react-router-dom'
 
 const Today = () => {
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 300);
+    }, []);
+
+    const todayPageClass = loading ? 'isLoading' : 'isLoaded';
+
     return (
         <Main 
             title = "추천 영상"
             description="오늘의 추천 유튜브 영상입니다.">
             
-            <section id='todayPage'>
+            <section id='todayPage' className={todayPageClass}>
                 <h2>🥰 오늘의 추천 영상입니다.</h2>
 
                 {todayText.map((today, key) => (
@@ -23,16 +33,12 @@ const Today = () => {
                         <div className='today__text'>
                             <span className='today'>today!</span>
                             <h3 className='title'>
-                                <Link to={today.page}>
-                                    {today.title}
-                                </Link>
+                                <Link to={today.page}>{today.title}</Link>
                             </h3>
                             <p className='desc'>{today.desc}</p>
                             <div className='info'>
                                 <span className='author'>
-                                    <Link to={`/channel/${today.channelId}`}>
-                                        {today.author}
-                                    </Link>
+                                    <Link to={`/channel/${today.channelId}`}>{today.author}</Link>
                                 </span>
                                 <span className='date'>{today.date}</span>
                             </div>
