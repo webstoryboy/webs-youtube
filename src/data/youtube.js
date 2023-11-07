9e922da2a3253da219439534019b1ba1e3c08a8c
웹스토리보이 <webstupids@gmail.com>
2023-11-07 13:59:42 +0900

    2023.11.07 이미지 최적화 작업

--- src/data/youtube.js
+++ src/data/youtube.js
@@ -1,148 +1,187 @@
-import youtube01 from "../assets/img/youtube/youtube01.jpg";
-import youtube02 from "../assets/img/youtube/youtube02.jpg";
-import youtube03 from "../assets/img/youtube/youtube03.jpg";
-import youtube04 from "../assets/img/youtube/youtube04.jpg";
-import youtube05 from "../assets/img/youtube/youtube05.jpg";
+import youtube01 from "../assets/img/youtube/youtube01-min.jpg";
+import youtube02 from "../assets/img/youtube/youtube02-min.jpg";
+import youtube03 from "../assets/img/youtube/youtube03-min.jpg";
+import youtube04 from "../assets/img/youtube/youtube04-min.jpg";
+import youtube05 from "../assets/img/youtube/youtube05-min.jpg";
+import youtube06 from "../assets/img/youtube/youtube06-min.jpg";
+import youtube07 from "../assets/img/youtube/youtube07-min.jpg";
+import youtube08 from "../assets/img/youtube/youtube08-min.jpg";
+import youtube09 from "../assets/img/youtube/youtube09-min.jpg";
+import youtube10 from "../assets/img/youtube/youtube10-min.jpg";
+import youtube11 from "../assets/img/youtube/youtube11-min.jpg";
+import youtube12 from "../assets/img/youtube/youtube12-min.jpg";
+import youtube13 from "../assets/img/youtube/youtube13-min.jpg";
+import youtube14 from "../assets/img/youtube/youtube14-min.jpg";
+import youtube15 from "../assets/img/youtube/youtube15-min.jpg";
+import youtube16 from "../assets/img/youtube/youtube16-min.jpg";
+import youtube17 from "../assets/img/youtube/youtube17-min.jpg";
+import youtube18 from "../assets/img/youtube/youtube18-min.jpg";
+import youtube19 from "../assets/img/youtube/youtube19-min.jpg";
+import youtube20 from "../assets/img/youtube/youtube20-min.jpg";
+import youtube21 from "../assets/img/youtube/youtube21-min.jpg";
+import youtube22 from "../assets/img/youtube/youtube22-min.jpg";
+import youtube23 from "../assets/img/youtube/youtube23-min.jpg";
 
 export const youtubeText = [
     {
-        title: "01 나만의 유튜브 사이트 만들기",
+        title: "01 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 소개 및 셋팅하기",
         img: youtube01,
         author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        videoId: "Yoqm5ah7qw4",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "02 나만의 유튜브 사이트 만들기",
+    }, {
+        title: "02 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 라이브러리 설치하기",
         img: youtube02,
         author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        videoId: "WEn4_u7iDSg",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "03 나만의 유튜브 사이트 만들기",
+    }, {
+        title: "03 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : Git 연동하기",
         img: youtube03,
         author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        videoId: "nYp9Itw0jko",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "04 나만의 유튜브 사이트 만들기",
+    }, {
+        title: "04 리액트(React.js)로 나만의 유튜브 사이트 만들기 : SCSS 셋팅하기",
         img: youtube04,
         author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        videoId: "j4S3Uw-L3xI",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "05 나만의 유튜브 사이트 만들기",
+    }, {
+        title: "05 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 페이지 만들기",
         img: youtube05,
         author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        videoId: "59lkbssnsPI",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "06 나만의 유튜브 사이트 만들기",
-        img: youtube01,
+    }, {
+        title: "06 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 섹션 컴퍼넌트 구조화하기",
+        img: youtube06,
+        author: "webstoryboy",
+        videoId: "K7YEZAI8_R4",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "07 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 헤더 영역 완성하기",
+        img: youtube07,
+        author: "webstoryboy",
+        videoId: "qO9usAVNj9Y",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "08 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 헤더 영역 데이터 작업",
+        img: youtube08,
+        author: "webstoryboy",
+        videoId: "5E6hIddRKqY",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "09 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 컴퍼넌트 비동기 작업",
+        img: youtube09,
+        author: "webstoryboy",
+        videoId: "5B5c7du4uzc",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "10 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 페이지 SEO 작업",
+        img: youtube10,
+        author: "webstoryboy",
+        videoId: "BFRV2LXb6HI",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "11 리액트(React.js)로 나만의 유튜브 사이트 만들기 : 메인 콘텐츠 작업",
+        img: youtube11,
+        author: "webstoryboy",
+        videoId: "OXO5lOGRS6s",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "12 리액트(React.js)로 나만의 유튜브 사이트 만들기 : 추천 영상 작업",
+        img: youtube12,
+        author: "webstoryboy",
+        videoId: "x0y9A9qhgk8",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "13 리액트(React.js)로 나만의 유튜브 사이트 만들기 : 추천 개발자 작업",
+        img: youtube13,
+        author: "webstoryboy",
+        videoId: "z8h7FoJ4xx0",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "14 리액트(React.js)로 나만의 유튜브 사이트 만들기 : 메인 섹션 나머지 콘텐츠 작업",
+        img: youtube14,
+        author: "webstoryboy",
+        videoId: "O0xeh6UOzjw",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "15 리액트(React.js)로 나만의 유튜브 사이트 만들기 : 비디오 컴퍼넌트 통합 작업",
+        img: youtube15,
+        author: "webstoryboy",
+        videoId: "7kvEG5Pst5w",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "16 리액트(React.js)로 나만의 유튜브 사이트 만들기 : Swiper 이미지 슬라이드 작업",
+        img: youtube16,
+        author: "webstoryboy",
+        videoId: "MD4X56S3h0Q",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "17 리액트(React.js)로 나만의 유튜브 사이트 만들기 : 나머지 페이지 작업",
+        img: youtube17,
+        author: "webstoryboy",
+        videoId: "bOch_BC8GR0",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "18 리액트(React.js)로 나만의 유튜브 사이트 만들기 : 검색 및 유튜브 API 설정하기",
+        img: youtube18,
+        author: "webstoryboy",
+        videoId: "OSaxhjgHE50",
+        date: "2023.09.09",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    }, {
+        title: "19 리액트(React.js)로 나만의 유튜브 사이트 만들기 : Rapid API 및 더보기 설정하기",
+        img: youtube19,
         author: "webstoryboy",
         videoId: "OKZXYgAgtyw",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "07 나만의 유튜브 사이트 만들기",
-        img: youtube02,
+    }, {
+        title: "20 리액트(React.js)로 나만의 유튜브 사이트 만들기 : 비디오 상세 페이지 만들기",
+        img: youtube20,
         author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        videoId: "3DaT04GrF9I",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "08 나만의 유튜브 사이트 만들기",
-        img: youtube03,
+    }, {
+        title: "21 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 채널 상세 페이지 만들기",
+        img: youtube21,
         author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        videoId: "nZOugO3TTgw",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "09 나만의 유튜브 사이트 만들기",
-        img: youtube04,
+    }, {
+        title: "22 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 버그 고치기",
+        img: youtube22,
         author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        videoId: "qV5gRfnPf_I",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "10 나만의 유튜브 사이트 만들기",
-        img: youtube05,
+    }, {
+        title: "23 리액트(React.js)로 나만의 유튜브 사이트 만들기 프로젝트 : 빌드 및 배포하기",
+        img: youtube23,
         author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "11 나만의 유튜브 사이트 만들기",
-        img: youtube01,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "12 나만의 유튜브 사이트 만들기",
-        img: youtube02,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "13 나만의 유튜브 사이트 만들기",
-        img: youtube03,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "14 나만의 유튜브 사이트 만들기",
-        img: youtube04,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "15 나만의 유튜브 사이트 만들기",
-        img: youtube05,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "16 나만의 유튜브 사이트 만들기",
-        img: youtube01,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "17 나만의 유튜브 사이트 만들기",
-        img: youtube02,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "18 나만의 유튜브 사이트 만들기",
-        img: youtube03,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "19 나만의 유튜브 사이트 만들기",
-        img: youtube04,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "20 나만의 유튜브 사이트 만들기",
-        img: youtube05,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        videoId: "b1eQ3MRwfBw",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     }
